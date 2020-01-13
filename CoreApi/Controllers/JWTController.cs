80bb8635670e13646c4b1693f647dc056aa91b16
guozhongxiang <guozhongxiang@DESKTOP-HQLML1F>
2020-01-13 11:53:13 +0800

    identityserver未完成

---
+++ CoreApi/Controllers/JWTController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JWTController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "index";
+        }
+    }
+}
---
+++ IdentityClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdentityClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
