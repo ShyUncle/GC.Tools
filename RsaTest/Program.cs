88028e12f9ad334dd6dc36ce26e3f2d82380249a
guozhongxiang <shyuncle_gzx@163.com>
2020-03-08 00:00:39 +0800

    ss

---
+++ RsaTest/Program.cs
@@ -0,0 +1,202 @@
+﻿using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RsaTest
+{
+    class Program
+    {
+        private AsymmetricKeyParameter GetPublicKeyParameter(string s)
+        {
+            s = s.Replace("\r", "").Replace("\n", "").Replace(" ", "");
+            byte[] publicInfoByte = Convert.FromBase64String(s);
+            Asn1Object pubKeyObj = Asn1Object.FromByteArray(publicInfoByte);//这里也可以从流中读取，从本地导入   
+            AsymmetricKeyParameter pubKey = PublicKeyFactory.CreateKey(publicInfoByte);
+            return pubKey;
+        }
+        static void Main(string[] args)
+        {
+            var str = File.ReadAllText(AppContext.BaseDirectory + "public_pkcs1.pem");
+          var a= PemToRSAKey(str, false);
+            var b = RsaEncryptWithPublic("abc",str);
+            //RSA密钥对的构造器  
+            RsaKeyPairGenerator keyGenerator = new RsaKeyPairGenerator();
+          
+            //RSA密钥构造器的参数  
+            RsaKeyGenerationParameters param = new RsaKeyGenerationParameters(
+                Org.BouncyCastle.Math.BigInteger.ValueOf(3),
+                new Org.BouncyCastle.Security.SecureRandom(),
+                1024,   //密钥长度  
+                25);
+            //用参数初始化密钥构造器  
+            keyGenerator.Init(param);
+            //产生密钥对  
+            AsymmetricCipherKeyPair keyPair = keyGenerator.GenerateKeyPair();
+            //获取公钥和密钥  
+            AsymmetricKeyParameter publicKey = keyPair.Public;
+            AsymmetricKeyParameter privateKey = keyPair.Private;
+            if (((RsaKeyParameters)publicKey).Modulus.BitLength < 1024)
+            {
+                Console.WriteLine("failed key generation (1024) length test");
+            }
+         
+            //一个测试……………………  
+            //输入，十六进制的字符串，解码为byte[]  
+            //string input = "4e6f77206973207468652074696d6520666f7220616c6c20676f6f64206d656e";  
+            //byte[] testData = Org.BouncyCastle.Utilities.Encoders.Hex.Decode(input);             
+            string input = "popozh RSA test";
+            byte[] testData = Encoding.UTF8.GetBytes(input);
+            Console.WriteLine("明文:" + input + Environment.NewLine);
+            //非对称加密算法，加解密用  
+            IAsymmetricBlockCipher engine = new RsaEngine();
+            //公钥加密  
+            engine.Init(true, publicKey);
+            try
+            {
+                testData = engine.ProcessBlock(testData, 0, testData.Length);
+                Console.WriteLine("密文（base64编码）:" + Convert.ToBase64String(testData) + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("failed - exception " + Environment.NewLine + ex.ToString());
+            }
+            //私钥解密  
+            engine.Init(false, privateKey);
+            try
+            {
+                testData = engine.ProcessBlock(testData, 0, testData.Length);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed - exception " + e.ToString());
+            }
+            if (input.Equals(Encoding.UTF8.GetString(testData)))
+            {
+                Console.WriteLine("解密成功");
+            }
+            Console.Read();
+            
+        }
+
+        /// <summary>
+        /// RSA密钥转Pem密钥
+        /// </summary>
+        /// <param name="RSAKey">RSA密钥</param>
+        /// <param name="isPrivateKey">是否是私钥</param>
+        /// <returns>Pem密钥</returns>
+        public static string RSAKeyToPem(string RSAKey, bool isPrivateKey)
+        {
+            string pemKey = string.Empty;
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(RSAKey);
+            RSAParameters rsaPara = new RSAParameters();
+            RsaKeyParameters key = null;
+            //RSA私钥
+            if (isPrivateKey)
+            {
+                rsaPara = rsa.ExportParameters(true);
+                key = new RsaPrivateCrtKeyParameters(
+                    new BigInteger(1, rsaPara.Modulus), new BigInteger(1, rsaPara.Exponent), new BigInteger(1, rsaPara.D),
+                    new BigInteger(1, rsaPara.P), new BigInteger(1, rsaPara.Q), new BigInteger(1, rsaPara.DP), new BigInteger(1, rsaPara.DQ),
+                    new BigInteger(1, rsaPara.InverseQ));
+            }
+            //RSA公钥
+            else
+            {
+                rsaPara = rsa.ExportParameters(false);
+                key = new RsaKeyParameters(false,
+                    new BigInteger(1, rsaPara.Modulus),
+                    new BigInteger(1, rsaPara.Exponent));
+            }
+            using (TextWriter sw = new StringWriter())
+            {
+                var pemWriter = new Org.BouncyCastle.OpenSsl.PemWriter(sw);
+                pemWriter.WriteObject(key);
+                pemWriter.Writer.Flush();
+                pemKey = sw.ToString();
+            }
+            return pemKey;
+        }
+        /// <summary>
+        /// Pem密钥转RSA密钥
+        /// </summary>
+        /// <param name="pemKey">Pem密钥</param>
+        /// <param name="isPrivateKey">是否是私钥</param>
+        /// <returns>RSA密钥</returns>
+        public static string PemToRSAKey(string pemKey, bool isPrivateKey)
+        {
+            string rsaKey = string.Empty;
+            object pemObject = null;
+            RSAParameters rsaPara = new RSAParameters();
+            using (StringReader sReader = new StringReader(pemKey))
+            {
+                var pemReader = new Org.BouncyCastle.OpenSsl.PemReader(sReader);
+                pemObject = pemReader.ReadObject();
+            }
+            //RSA私钥
+            if (isPrivateKey)
+            {
+                RsaPrivateCrtKeyParameters key = (RsaPrivateCrtKeyParameters)((AsymmetricCipherKeyPair)pemObject).Private;
+                rsaPara = new RSAParameters
+                {
+                    Modulus = key.Modulus.ToByteArrayUnsigned(),
+                    Exponent = key.PublicExponent.ToByteArrayUnsigned(),
+                    D = key.Exponent.ToByteArrayUnsigned(),
+                    P = key.P.ToByteArrayUnsigned(),
+                    Q = key.Q.ToByteArrayUnsigned(),
+                    DP = key.DP.ToByteArrayUnsigned(),
+                    DQ = key.DQ.ToByteArrayUnsigned(),
+                    InverseQ = key.QInv.ToByteArrayUnsigned(),
+                };
+            }
+            //RSA公钥
+            else
+            {
+                RsaKeyParameters key = (RsaKeyParameters)pemObject;
+                rsaPara = new RSAParameters
+                {
+                    Modulus = key.Modulus.ToByteArrayUnsigned(),
+                    Exponent = key.Exponent.ToByteArrayUnsigned(),
+                };
+            }
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.ImportParameters(rsaPara);
+            using (StringWriter sw = new StringWriter())
+            {
+                sw.Write(rsa.ToXmlString(isPrivateKey ? true : false));
+                rsaKey = sw.ToString();
+            }
+            return rsaKey;
+        }
+
+        public static string RsaEncryptWithPublic(string clearText
+       , string publicKey)
+        {
+            var bytesToEncrypt = Encoding.UTF8.GetBytes(clearText);
+
+            var encryptEngine = new Pkcs1Encoding(new RsaEngine());
+
+            using (var txtreader = new StringReader(publicKey))
+            {
+                var keyParameter = (AsymmetricKeyParameter)new PemReader(txtreader).ReadObject();
+
+                encryptEngine.Init(true, keyParameter);
+            }
+
+            var encrypted = Convert.ToBase64String(encryptEngine.ProcessBlock(bytesToEncrypt, 0, bytesToEncrypt.Length));
+            return encrypted;
+
+        }
+    }
+}
