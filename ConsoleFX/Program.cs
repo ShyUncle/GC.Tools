f06b63993d4bb060a5d0a8151ad8c9798fa87b69
guozhongxiang <guozhongxiang@DESKTOP-HQLML1F>
2020-03-19 17:42:15 +0800

    asp.net core identity

--- ConsoleFX/Program.cs
+++ ConsoleFX/Program.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace ConsoleFX
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ImageHelper.CompressImage(AppDomain.CurrentDomain.BaseDirectory+ "\\.net core学习路线图.png", AppDomain.CurrentDomain.BaseDirectory + "\\a.png");
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://localhost:9999/store/PayQrCode?orderId=24&payMethod=2");
+            request.Method = "Get";
+            using (var stream = request.GetResponse().GetResponseStream())
+            {
+                using (FileStream file = new FileStream("abc.png", FileMode.Create))
+                {
+                    stream.CopyTo(file);
+                    file.Flush();
+                }
+            }
+
+            ImageHelper.CompressImage(AppDomain.CurrentDomain.BaseDirectory + "\\.net core学习路线图.png", AppDomain.CurrentDomain.BaseDirectory + "\\a.png");
         }
     }
 }
